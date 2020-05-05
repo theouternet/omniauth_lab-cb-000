82bc099c0d81a92e0d74f5f955b8e224c19fc2a6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 23:43:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Add your routes here
+  
+  root: 'welcome/home'
+  get 
+  
     match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
 end
